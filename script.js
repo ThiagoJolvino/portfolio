021101ca3236dd7dd035bdd084c8bc55614b9572
thiago jolvino <thiagojolvino@gmail.com>
2021-03-04 19:43:27 -0300

    :up: responsividade

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-
-
 document.querySelector(".hamburguer").addEventListener("click", () =>{
     document.querySelector(".container").classList.toggle("show-menu")
 });
+
+document.querySelector("#qtde").addEventListener("change", atualizarPreco)
+document.querySelector("#js").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-sim").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-nao").addEventListener("change", atualizarPreco)
+document.querySelector("#prazo").addEventListener("change", function(){
+    const prazo = document.querySelector("#prazo").value
+    if(prazo != 1){
+    document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`
+}else{
+    document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semana`
+}
+    atualizarPreco()
+})
+
+function atualizarPreco(){
+    const qtde = document.querySelector("#qtde").value
+    const temJs = document.querySelector("#js").checked
+    const incluiLayout = document.querySelector("#layout-sim").checked
+    const prazo = document.querySelector("#prazo").value
+
+
+    let preco = qtde * 100;
+    if (temJs) preco *= 1.1
+    if(incluiLayout) preco += 500
+    let taxaUrgencia = 1 - prazo*0.1;
+    preco *= 1 + taxaUrgencia
+    
+    document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`;
+}
